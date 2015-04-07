6ec88944a100d8a4746ba5d46dc9da2efd40b79d
Nathan Brewer <nathan.brewer@rightscale.com>
2015-04-07 13:31:43 -0700

    Only rewind of credentials are provided

--- recipes/master.rb
+++ recipes/master.rb
@@ -7,17 +7,22 @@ node.override['rs-mysql']['dns']['master_fqdn'] = master_fqdn
 
 include_recipe "rs-mysql::master"
 
-dns_name = master_fqdn.split('.').first
-
-rewind :dns => dns_name  do
-  provider 'dns_dnsmadeeasy_api20'
-  domain domain_name
-  credentials(
-    'dnsmadeeasy_api_key' => node['rs-mysql']['dns']['user_key'],
-    'dnsmadeeasy_secret_key' => node['rs-mysql']['dns']['secret_key'],
-  )
-  entry_name node['slice_rs-mysql']['dns']['hostname']
-  entry_value node['mysql']['bind_address']
-  type node['dns']['entry']['type']
-  ttl 60
+missing_dns_creds = RsMysql::Helper.find_missing_dns_credentials(node)
+if missing_dns_creds.empty?
+  dns_name = master_fqdn.split('.').first
+  rewind :dns => dns_name  do
+    provider 'dns_dnsmadeeasy_api20'
+    domain node['slice_rs-mysql']['dns']['domain']
+    credentials(
+      'dnsmadeeasy_api_key' => node['rs-mysql']['dns']['user_key'],
+      'dnsmadeeasy_secret_key' => node['rs-mysql']['dns']['secret_key'],
+    )
+    entry_name node['slice_rs-mysql']['dns']['hostname']
+    entry_value node['mysql']['bind_address']
+    type node['dns']['entry']['type']
+    ttl 60
+  end
+else
+  missing_dns_creds.map! { |cred| "rs-mysql/dns/#{cred}" }
+  log "Following DNS credentials are missing #{missing_dns_creds.join(', ')}! Skipping DNS setting..."
 end
